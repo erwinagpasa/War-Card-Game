8ed3cc8111618c94e594585aa9b5a68a27a5f3a8
erwinagpasa <rootaccess000@gmail.com>
2024-03-02 08:05:30 +0300

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Developer on 02/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
