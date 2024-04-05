17d2d9bfb6b09e2a6c34225896f511751f9d9014
erwinagpasa <rootaccess000@gmail.com>
2024-04-05 08:32:58 +0300

    adding butoon functionality

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -9,13 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+          Image("background-cloth")
+            .resizable()
+            .ignoresSafeArea()
+          
+          VStack {
+            Spacer()
+            Image("logo")
+            Spacer()
+            
+            HStack {
+              Spacer()
+              Image("card2")
+              Spacer()
+              Image("card3")
+              Spacer()
+            }
+           
+            Spacer()
+            
+            Button("Deal") {
+              print("Deal Cards")
+            }
+            
+            Spacer()
+            
+            
+            
+            HStack {
+              Spacer()
+              VStack {
+                Text("Player")
+                  .font(.headline)
+                Text("0")
+                  .font(.largeTitle)
+              }
+              Spacer()
+              VStack {
+                Text("Player")
+                  .font(.headline)
+                Text("0")
+                  .font(.largeTitle)
+              }
+              Spacer()
+            }
+            .foregroundColor(.white)
+            Spacer()
+                      
+          }
         }
-        .padding()
     }
 }
 
